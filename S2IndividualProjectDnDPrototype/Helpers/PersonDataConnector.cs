dfc65f1e0bc245d2a43b79c9b51d0b5f88dda3ef
FrowKarim <57719623+FrowKarim@users.noreply.github.com>
2025-11-11 11:56:16 +0100

    Added functionality to the details page
    Details page now properly reflects the correct user card that got clicked on.

--- S2IndividualProjectDnDPrototype/Helpers/PersonDataConnector.cs
+++ S2IndividualProjectDnDPrototype/Helpers/PersonDataConnector.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using Azure.Core;
+using Microsoft.Data.SqlClient;
 using Microsoft.Identity.Client;
 using S2IndividualProjectDnDPrototype.Models;
 
@@ -20,29 +21,38 @@ namespace S2IndividualProjectDnDPrototype.Helpers
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
-                using (SqlCommand sqlcommand = new SqlCommand("SELECT * FROM Person WHERE Name='Frow' ", connection))
-                {
-
-                    using (SqlDataReader reader = sqlcommand.ExecuteReader())
+                
+                    using (SqlCommand sqlcommand = new SqlCommand("SELECT * FROM Person WHERE Name='Frow' ", connection))
                     {
-                        while (reader.Read())
+
+                        using (SqlDataReader reader = sqlcommand.ExecuteReader())
                         {
+                            while (reader.Read())
+                            {
 
-                            person.Id = Convert.ToInt32(reader["ID"]);
-                            person.Name = reader["Name"].ToString();
-                            person.Age = Convert.ToInt32(reader["Age"]);
-                            person.Gender = reader["Gender"].ToString();
-                            person.Address = reader["Address"].ToString();
+                                person.Id = Convert.ToInt32(reader["ID"]);
+                                person.Name = reader["Name"].ToString();
+                                person.Age = Convert.ToInt32(reader["Age"]);
+                                person.Gender = reader["Gender"].ToString();
+                                person.Address = reader["Address"].ToString();
 
+                            }
                         }
+
+
                     }
-
-
-                }
-
+                
             }
 
-
+       
+        
+        //    using (MySqlConnection connection = new MySqlConnection(connectionString))
+        //    {
+        //        connection.Open();
+        //        using (MySqlCommand command = new MySqlCommand("SELECT * FROM student WHERE name = @name", connection))
+        //        {
+        //            command.Parameters.AddWithValue("@name", Request.Query["name"].ToString());
+   
 
             return person;
         }
--- S2IndividualProjectDnDPrototype/Pages/PersonPages/Details.cshtml.cs
+++ S2IndividualProjectDnDPrototype/Pages/PersonPages/Details.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using S2IndividualProjectDnDPrototype.Helpers;
 using S2IndividualProjectDnDPrototype.Helpers;
 using S2IndividualProjectDnDPrototype.Models;
 
@@ -11,10 +13,58 @@ namespace S2IndividualProjectDnDPrototype.Pages.PersonPages
 
         public void OnGet()
         {
-
+            
             PersonDataConnector conn = new PersonDataConnector();
-
+            Person SinglePerson = conn.GetPerson();
             //People = conn.GetPeople();
+        }
+
+        public Person GetPerson()
+        {
+            Person person = new Person();
+
+            string connectionString = ("Server=mssqlstud.fhict.local;" +
+                                "Database=dbi439179_test;" +
+                                "User Id=dbi439179_test;" +
+                                "Password=MSSQL; " +
+                                "TrustServerCertificate = true");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand sqlcommand = new SqlCommand("SELECT * FROM Person WHERE Name= @name ", connection))
+                {
+                    sqlcommand.Parameters.AddWithValue("@name", Request.Query["name"].ToString());
+                    using (SqlDataReader reader = sqlcommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+
+                            person.Id = Convert.ToInt32(reader["ID"]);
+                            person.Name = reader["Name"].ToString();
+                            person.Age = Convert.ToInt32(reader["Age"]);
+                            person.Gender = reader["Gender"].ToString();
+                            person.Address = reader["Address"].ToString();
+
+                        }
+                    }
+
+
+                }
+
+            }
+
+
+
+            //    using (MySqlConnection connection = new MySqlConnection(connectionString))
+            //    {
+            //        connection.Open();
+            //        using (MySqlCommand command = new MySqlCommand("SELECT * FROM student WHERE name = @name", connection))
+            //        {
+            //            command.Parameters.AddWithValue("@name", Request.Query["name"].ToString());
+
+
+            return person;
         }
     }
 }
