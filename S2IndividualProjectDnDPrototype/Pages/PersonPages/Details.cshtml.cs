142bf7e914523c0481bb7a8a59ac4969a2d7d339
FrowKarim <57719623+FrowKarim@users.noreply.github.com>
2025-11-12 10:09:38 +0100

    DB seperation from Details.cshtml.cs
    Successfully seperated database connectivity from the details class, now the PersonDataConnector is the only class that has connectivity with the database. Makes it easier to have a seperate DAL

--- S2IndividualProjectDnDPrototype/Pages/PersonPages/Details.cshtml.cs
+++ S2IndividualProjectDnDPrototype/Pages/PersonPages/Details.cshtml.cs
@@ -20,48 +20,49 @@ namespace S2IndividualProjectDnDPrototype.Pages.PersonPages
             //People = conn.GetPeople();
         }
 
-        public Person GetPerson()
-        {
-            //string userId = Request.Query["userID"].ToString();
-            //PersonDataConnector conn = new PersonDataConnector();
-            //return conn.GetPerson(userId);
+        
+        //public Person GetPerson()
+        //{
+        //    //string userId = Request.Query["userID"].ToString();
+        //    //PersonDataConnector conn = new PersonDataConnector();
+        //    //return conn.GetPerson(userId);
 
-            Person person = new Person();
+        //    Person person = new Person();
 
-            string connectionString = ("Server=mssqlstud.fhict.local;" +
-                                "Database=dbi439179_test;" +
-                                "User Id=dbi439179_test;" +
-                                "Password=MSSQL; " +
-                                "TrustServerCertificate = true");
+        //    string connectionString = ("Server=mssqlstud.fhict.local;" +
+        //                        "Database=dbi439179_test;" +
+        //                        "User Id=dbi439179_test;" +
+        //                        "Password=MSSQL; " +
+        //                        "TrustServerCertificate = true");
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-                using (SqlCommand sqlcommand = new SqlCommand("SELECT * FROM Person WHERE ID= @Id ", connection))
-                {
-                    sqlcommand.Parameters.AddWithValue("@Id", Request.Query["userID"].ToString());
-                    using (SqlDataReader reader = sqlcommand.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
+        //    using (SqlConnection connection = new SqlConnection(connectionString))
+        //    {
+        //        connection.Open();
+        //        using (SqlCommand sqlcommand = new SqlCommand("SELECT * FROM Person WHERE ID= @Id ", connection))
+        //        {
+        //            sqlcommand.Parameters.AddWithValue("@Id", Request.Query["userID"].ToString());
+        //            using (SqlDataReader reader = sqlcommand.ExecuteReader())
+        //            {
+        //                while (reader.Read())
+        //                {
 
-                            person.Id = Convert.ToInt32(reader["ID"]);
-                            person.Name = reader["Name"].ToString();
-                            person.Age = Convert.ToInt32(reader["Age"]);
-                            person.Gender = reader["Gender"].ToString();
-                            person.Address = reader["Address"].ToString();
+        //                    person.Id = Convert.ToInt32(reader["ID"]);
+        //                    person.Name = reader["Name"].ToString();
+        //                    person.Age = Convert.ToInt32(reader["Age"]);
+        //                    person.Gender = reader["Gender"].ToString();
+        //                    person.Address = reader["Address"].ToString();
 
-                        }
-                    }
-
-
-                }
-
-            }
+        //                }
+        //            }
 
 
-            return person;
-        }
+        //        }
+
+        //    }
+
+
+        //    return person;
+        //}
 
         public Person getSinglePerson()
         {
